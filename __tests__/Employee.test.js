ef824badeb7a56965d56c2665d604da5c2bfcf04
mcc <costanza+git@gmail.com>
2021-05-27 14:12:00 -0700

    implement engineer class, with tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,34 +1,34 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-  const employee = new Employee('Joe Workerman', '123456', 'joe@workerman.net');
+  const employee = new Employee('Joe Workerman', '123456', 'joe.workerman@thiscompany.net');
 
   expect(employee.name).toBe('Joe Workerman');
   expect(employee.id).toBe('123456');
-  expect(employee.email).toBe('joe@workerman.net');
+  expect(employee.email).toBe('joe.workerman@thiscompany.net');
   expect(employee.role).toBe('Employee');
 });
 
 test("gets employee's name", () => {
-  const employee = new Employee('Joe Workerman', '123456', 'joe@workerman.net');
+  const employee = new Employee('Joe Workerman', '123456', 'joe.workerman@thiscompany.net');
 
   expect(employee.getName()).toBe('Joe Workerman');
 });
 
 test("gets employee's id", () => {
-  const employee = new Employee('Joe Workerman', '123456', 'joe@workerman.net');
+  const employee = new Employee('Joe Workerman', '123456', 'joe.workerman@thiscompany.net');
 
   expect(employee.getId()).toBe('123456');
 });
 
 test("gets employee's email address", () => {
-  const employee = new Employee('Joe Workerman', '123456', 'joe@workerman.net');
+  const employee = new Employee('Joe Workerman', '123456', 'joe.workerman@thiscompany.net');
 
-  expect(employee.getEmail()).toBe('joe@workerman.net');
+  expect(employee.getEmail()).toBe('joe.workerman@thiscompany.net');
 });
 
 test("gets employee's role", () => {
-  const employee = new Employee('Joe Workerman', '123456', 'joe@workerman.net');
+  const employee = new Employee('Joe Workerman', '123456', 'joe.workerman@thiscompany.net');
 
   expect(employee.getRole()).toBe('Employee');
 });
